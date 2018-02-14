4757045ad5f6452859d9e440aeb6fef27e14ab48
warglaivee <warglaivegm@gmail.com>
2018-02-14 17:36:10 +0200

    0/100

---
+++ Defining Classes - Exercise/07.SpeedRacing/07.SpeedRacing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        var takeListCars = new List<Car>();
+        var n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            var carInfoInput = Console.ReadLine();
+            var carInfoArgs = carInfoInput.Split();
+
+            var currentCar = new Car();//car model is unique
+            currentCar.Model = carInfoArgs[0];
+            currentCar.FuelAmount = double.Parse(carInfoArgs[1]);
+            currentCar.FuelConsumptionFor1km = double.Parse(carInfoArgs[2]);
+            currentCar.DistanceTraveled = 0;
+            takeListCars.Add(currentCar);
+
+        }
+        var commands = Console.ReadLine();
+        //Drive the cars
+        while (commands != "End")
+        {
+            Car checkCar = new Car();
+            var commandParts = commands.Split();
+            var carModel = commandParts[1];
+            var amountOfKm = int.Parse(commandParts[2]);
+            checkCar.CanTheCarMoveDistance(takeListCars, carModel, amountOfKm);
+
+            commands = Console.ReadLine();
+        }
+        foreach (var currentCar in takeListCars)
+        {
+            Console.WriteLine($"{currentCar.Model} " +
+                              $"{currentCar.FuelAmount:f2}" +
+                              $" {currentCar.DistanceTraveled}");
+        }
+    }
+}
---
+++ Defining Classes - Exercise/07.SpeedRacing/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Car
+{
+    private string model;
+    private double fuelAmount;
+    private double fuelConsumptionFor1km;
+    private double distanceTraveled;
+
+    public string Model
+    {
+        get => model;
+        set => model = value;
+    }
+    public double FuelAmount
+    {
+        get => fuelAmount;
+        set => fuelAmount = value;
+    }
+    public double FuelConsumptionFor1km
+    {
+        get => fuelConsumptionFor1km;
+        set => fuelConsumptionFor1km = value;
+    }
+
+    public double DistanceTraveled
+    {
+        get => distanceTraveled;
+        set => distanceTraveled = value;
+    }
+    //list cars
+
+    public Car CanTheCarMoveDistance(List<Car> takeListCars, string model, double distanceToMove)
+    {
+        var filterModels = takeListCars
+            .Where(x => x.model == model)
+            .ToList();
+        var currentCar = filterModels[0];
+        //To do
+        var maxTravelDistance = currentCar.FuelAmount * currentCar.FuelConsumptionFor1km;
+
+        if (maxTravelDistance >= distanceToMove)
+        {
+            currentCar.FuelAmount -= currentCar.FuelAmount;
+            currentCar.DistanceTraveled += maxTravelDistance;
+
+        }
+        else
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+        return currentCar;
+    }
+}
