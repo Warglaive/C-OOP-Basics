7507d885638192263420b0438327ae172a9ad508
warglaive <warglaivegm@gmail.com>
2018-02-15 14:58:44 +0200

    to do lab

---
+++ Working with Abstraction Lab/01.RhombusofStars/01.RhombusofStars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        PrintRow(n);
+    }
+
+    public static void PrintRow(int n)
+    {
+        var spaces = 0;
+        var stars = 0;
+        for (int i = 1; i <= n * 2 - 1; i++)
+        {
+            spaces = n - i;
+        }
+    }
+}
