9f3ce75935b24cab2b81a07cb21ce124db87da2b
warglaive <warglaivegm@gmail.com>
2018-02-19 14:46:56 +0200

    01.Persons

---
+++ Encapsulation - Lab/01.Persons/01.Persons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        var linesCount = int.Parse(Console.ReadLine());
+        var persons = new List<Person>();
+        for (int index = 0; index < linesCount; index++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            var currentPerson = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+            persons.Add(currentPerson);
+        }
+
+        persons.OrderBy(p => p.FirstName)
+            .ThenBy(p => p.Age)
+            .ToList()
+            .ForEach(p => Console.WriteLine(p.ToString()));
+
+    }
+}
+
