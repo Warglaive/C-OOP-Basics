3c97fc6ae500905e116a5de785139bb949fe8b96
warglaivee <warglaivegm@gmail.com>
2018-02-20 19:54:17 +0200

    working with abstractions LAB done

--- Working with Abstraction Lab/04.HotelReservation/04.HotelReservation.cs
+++ Working with Abstraction Lab/04.HotelReservation/04.HotelReservation.cs
@@ -5,6 +5,31 @@ public class Program
 {
     public static void Main()
     {
+        var input = Console.ReadLine()
+            .Split();
+        var pricePerDay = decimal.Parse(input[0]);
+        var daysCount = int.Parse(input[1]);
 
+        var seasonType =
+            Enum.Parse<PriceCalculator.Season>(input[2]);
+        if (input.Length <= 3)
+        {
+            var discountType = PriceCalculator.DiscountType.None;
+
+            var calculate = new PriceCalculator();
+            var result = calculate.CalculatePrice(pricePerDay,
+                daysCount, seasonType, discountType);
+            Console.WriteLine($"{result:f2}");
+        }
+        else
+        {
+            var discountType =
+               Enum.Parse<PriceCalculator.DiscountType>(input[3]);
+
+            var calculate = new PriceCalculator();
+            var result = calculate.CalculatePrice(pricePerDay,
+                daysCount, seasonType, discountType);
+            Console.WriteLine($"{result:f2}");
+        }
     }
 }
--- Working with Abstraction Lab/04.HotelReservation/PriceCalculator.cs
+++ Working with Abstraction Lab/04.HotelReservation/PriceCalculator.cs
@@ -5,5 +5,67 @@ using System.Text;
 
 public class PriceCalculator
 {
+    private decimal pricePerDay;
+    private int daysCount;
 
+    public enum Season
+    {
+        Spring = 2,
+        Summer = 4,
+        Autumn = 1,
+        Winter = 3
+    }
+
+    public enum DiscountType
+    {
+        None,
+        SecondVisit = 10,
+        VIP = 20
+    }
+
+    public decimal CalculatePrice(decimal pricePerDay,
+        int daysCount, Season season, DiscountType discount)
+    {
+        //
+        var defaultPricePerDay = pricePerDay * daysCount;
+        var newPrice = 0m;
+        if (season == Season.Autumn)
+        {
+            newPrice = defaultPricePerDay *= (decimal)season;
+            newPrice = calculateDiscount(newPrice, discount, defaultPricePerDay);
+        }
+        else if (season == Season.Spring)
+        {
+            newPrice = defaultPricePerDay *= (decimal)season;
+            newPrice = calculateDiscount(newPrice, discount, defaultPricePerDay);
+        }
+        else if (season == Season.Summer)
+        {
+            newPrice = defaultPricePerDay *= (decimal)season;
+            newPrice = calculateDiscount(newPrice, discount, defaultPricePerDay);
+        }
+        else
+        {
+            newPrice = defaultPricePerDay *= (decimal)season;
+            newPrice = calculateDiscount(newPrice, discount, defaultPricePerDay);
+        }
+        return newPrice;
+    }
+
+    private static decimal calculateDiscount(decimal newPrice, DiscountType discount, decimal defaultPricePerDay)
+    {
+        switch (discount)
+        {
+            case DiscountType.VIP:
+                newPrice = defaultPricePerDay * 0.80m;
+                break;
+            case DiscountType.SecondVisit:
+                newPrice = defaultPricePerDay * 0.90m;
+                break;
+            case DiscountType.None:
+                //no discount
+                break;
+        }
+        return newPrice;
+    }
 }
