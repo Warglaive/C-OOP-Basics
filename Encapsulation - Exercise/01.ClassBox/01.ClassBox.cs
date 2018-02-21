99e1b230087fbbd5ab9fb68471f419290ef985b4
warglaivee <warglaivegm@gmail.com>
2018-02-21 13:00:23 +0200

    Encapsulation - Exercise

---
+++ Encapsulation - Exercise/01.ClassBox/01.ClassBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        var lenght = decimal.Parse(Console.ReadLine());
+        var width = decimal.Parse(Console.ReadLine());
+        var height = decimal.Parse(Console.ReadLine());
+
+        var currentBox = new Box();
+        var volumeResult = currentBox.CalculateVolume(lenght, width, height);
+        var lateralSurfaceAreaResult = currentBox.CalculateLateralSurfaceArea(lenght, width, height);
+        var surfaceArea = currentBox.CalculateSurfaceArea(lenght, width, height);
+        Console.WriteLine($"Surface Area - {surfaceArea:f2}" +
+                          $"{Environment.NewLine}" +
+                          $"Lateral Surface Area - {lateralSurfaceAreaResult:f2}" +
+                          $"{Environment.NewLine}" +
+                          $"Volume - {volumeResult:f2}");
+    }
+}
---
+++ Encapsulation - Exercise/01.ClassBox/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Box
+{
+    private decimal lenght;
+
+    private decimal Lenght
+    {
+        get { return lenght; }
+        set { lenght = value; }
+    }
+    private decimal width;
+
+    private decimal Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+    private decimal height;
+
+    private decimal Height
+    {
+        get { return height; }
+        set { height = value; }
+    }
+
+
+    public decimal CalculateLateralSurfaceArea(decimal lenght, decimal width, decimal height)
+    {
+        var result = 2 * (lenght * height) + 2 * (width * height);
+        return result;
+    }
+    public decimal CalculateVolume(decimal lenght, decimal width, decimal height)
+    {
+
+        var result = lenght * width * height;
+        return result;
+    }
+    public decimal CalculateSurfaceArea(decimal lenght, decimal width, decimal height)
+    {//2lw + 2lh + 2wh
+
+        var result = 2 * (lenght * width) + 2 * (lenght * height) + 2 * (width * height);
+        return result;
+    }
+}
