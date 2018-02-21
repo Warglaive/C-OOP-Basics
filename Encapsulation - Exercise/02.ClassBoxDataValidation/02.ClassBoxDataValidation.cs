e2779eba27a25868ab9ff3a95fe34e0518d09553
warglaivee <warglaivegm@gmail.com>
2018-02-21 14:08:35 +0200

    up

---
+++ Encapsulation - Exercise/02.ClassBoxDataValidation/02.ClassBoxDataValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var lenght = decimal.Parse(Console.ReadLine());
+        var width = decimal.Parse(Console.ReadLine());
+        var height = decimal.Parse(Console.ReadLine());
+        var currentBox = new Box(lenght, width, height);
+        var volumeResult = currentBox.CalculateVolume(lenght, width, height);
+        var lateralSurfaceAreaResult = currentBox.CalculateLateralSurfaceArea(lenght, width, height);
+        var surfaceArea = currentBox.CalculateSurfaceArea(lenght, width, height);
+        if (volumeResult > 0
+            || lateralSurfaceAreaResult > 0
+            || surfaceArea > 0)
+        {
+            Console.WriteLine($"Surface Area - {surfaceArea:f2}" +
+                              $"{Environment.NewLine}" +
+                              $"Lateral Surface Area - {lateralSurfaceAreaResult:f2}" +
+                              $"{Environment.NewLine}" +
+                              $"Volume - {volumeResult:f2}");
+        }
+    }
+}
---
+++ Encapsulation - Exercise/02.ClassBoxDataValidation/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Box
+{
+    private decimal lenght;
+    private decimal width;
+    private decimal height;
+
+    public Box(decimal lenght, decimal width, decimal height)
+    {
+        this.Lenght = lenght;
+        this.Width = width;
+        Height = height;
+    }
+
+    private decimal Height
+    {
+        get => height;
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Height cannot be zero or negative.");
+            }
+            height = value;
+        }
+    }
+
+    private decimal Width
+    {
+        get => this.width;
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Width cannot be zero or negative.");
+            }
+            width = value;
+        }
+    }
+
+    private decimal Lenght
+    {
+        get => lenght;
+        set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Length cannot be zero or negative.");
+            }
+            lenght = value;
+        }
+    }
+
+    public decimal CalculateVolume(decimal lenght, decimal width, decimal height)
+    {
+        var result = lenght * width * height;
+        return result;
+    }
+    public decimal CalculateSurfaceArea(decimal lenght, decimal width, decimal height)
+    {
+        var result = 2 * (lenght * width) + 2
+            * (lenght * height) + 2 * (width * height);
+        return result;
+    }
+    public decimal CalculateLateralSurfaceArea(decimal lenght, decimal width, decimal height)
+    {
+        var result = 2 * (lenght * height) + 2 * (width * height);
+        return result;
+    }
+}
