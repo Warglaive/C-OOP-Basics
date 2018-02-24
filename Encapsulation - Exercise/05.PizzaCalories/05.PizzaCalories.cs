1dbc737ed04e95a8ea4a5e316abfb04a705b0865
warglaivee <warglaivegm@gmail.com>
2018-02-24 15:34:17 +0200

    05.PizzaCalories 2/6 done

---
+++ Encapsulation - Exercise/05.PizzaCalories/05.PizzaCalories.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine();
+        while (input != "END")
+        {
+            try
+            {
+                string flourType;
+                string bakingTechnique;
+                decimal weight;
+                var name = ReadInput(input, out flourType, out bakingTechnique, out weight);
+                CalculatePrintTotalCalories(name, flourType, bakingTechnique, weight);
+                input = Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(0);
+            }
+        }
+    }
+
+    private static void CalculatePrintTotalCalories(string name, string flourType, string bakingTechnique, decimal weight)
+    {
+        var totalCalories = new Dough(name, flourType, bakingTechnique, weight);
+        Console.WriteLine($"{totalCalories.CalculateTotalCalories(flourType, bakingTechnique, weight):f2}");
+    }
+
+    private static string ReadInput(string input, out string flourType, out string bakingTechnique, out decimal weight)
+    {
+        var inputArgs = input.Split(new[] { ' ' }
+            , StringSplitOptions.RemoveEmptyEntries);
+        var name = inputArgs[0];
+        flourType = inputArgs[1];
+        bakingTechnique = inputArgs[2];
+        weight = decimal.Parse(inputArgs[3]);
+        return name;
+    }
+}
+
---
+++ Encapsulation - Exercise/05.PizzaCalories/Dough.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Dough
+{
+    private string name;
+    private string flourType;
+    private string bakingTechnique;
+    private decimal weight;
+
+    public Dough(string name, string flourType
+        , string bakingTechnique, decimal weight)
+    {
+        this.Name = name;
+        this.FlourType = flourType;
+        this.BakingTechnique = bakingTechnique;
+        this.Weight = weight;
+        CalculateTotalCalories(flourType, bakingTechnique, weight);
+    }
+
+    public decimal Weight
+    {
+        get { return weight; }
+        set
+        {
+            if (value < 1 || value > 200)
+            {
+                throw new ArgumentException("Dough weight should be in the range [1..200].");
+            }
+            weight = value;
+        }
+    }
+
+    public string BakingTechnique
+    {
+        get { return bakingTechnique; }
+        set
+        {
+            if (value != "Crispy"
+                && value != "Chewy"
+                && value != "Homemade")
+            {
+                throw new ArgumentException("Invalid type of dough.");
+            }
+            bakingTechnique = value;
+        }
+    }
+
+    public string FlourType
+    {
+        get { return flourType; }
+        set
+        {
+            if (value != "White" && value != "Wholegrain")
+            {
+                throw new ArgumentException("Invalid type of dough.");
+            }
+            flourType = value;
+        }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public decimal CalculateTotalCalories(string flourType, string bakingTechnique, decimal weight)
+    {
+
+        weight *= 2;
+        switch (flourType)
+        {
+            case "White":
+                weight *= 1.5m;
+                break;
+            case "Wholegrain":
+                weight *= 1.0m;
+                break;
+        }
+        switch (bakingTechnique)
+        {
+            case "Crispy":
+                weight *= 0.9m;
+                break;
+            case "Chewy":
+                weight *= 1.1m;
+                break;
+            case "Homemade":
+                weight *= 1.0m;
+                break;
+        }
+        return weight;
+    }
+}
+
