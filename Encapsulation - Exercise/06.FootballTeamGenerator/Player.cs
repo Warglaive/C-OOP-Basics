fff2c39bf27254cd8ed65f72f7448021f6155ea5
warglaivee <warglaivegm@gmail.com>
2018-02-26 11:11:56 +0200

    to do

---
+++ Encapsulation - Exercise/06.FootballTeamGenerator/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Player
+{
+    private string name;
+    private List<int> averageStats;
+
+    public Player()
+    {
+        this.AverageStats = new List<int>();
+    }
+
+    public Player(string name) : this()
+    {
+        this.Name = name;
+    }
+
+    public List<int> AverageStats
+    {
+        get { return averageStats; }
+        set { averageStats = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+}
---
+++ Encapsulation - Exercise/06.FootballTeamGenerator/Team.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Team
+{
+    private int numberOfPlayers;
+    private string teamName;
+    private double teamRating;
+
+    private Dictionary<string, int> TeamResult { get; set; }
+
+    public Team()
+    {
+        this.TeamResult = new Dictionary<string, int>();
+    }
+
+    public Team(string teamName)
+    {
+        this.TeamName = teamName;
+    }
+
+    public double TeamRating
+    {
+        get { return teamRating; }
+        set { teamRating = value; }
+    }
+
+    public string TeamName
+    {
+        get { return teamName; }
+        set { teamName = value; }
+    }
+
+    public int NumberOfPlayers
+    {
+        get { return numberOfPlayers; }
+        set { numberOfPlayers = value; }
+    }
+}
