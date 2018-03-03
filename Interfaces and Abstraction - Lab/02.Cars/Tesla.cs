b984968f42a242b8444ae31113dd3080eadddf64
warglaivee <warglaivegm@gmail.com>
2018-03-03 17:22:10 +0200

    lab up

---
+++ Interfaces and Abstraction - Lab/02.Cars/Tesla.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Tesla : ICar, IElectricCar
+{
+    public string Model { get; }
+    public string Color { get; }
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+    public int Battery { get; }
+
+    public Tesla(string model, string color, int battery)
+    {
+        this.Model = model;
+        this.Color = color;
+        this.Battery = battery;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Color} Tesla {this.Model} with {this.Battery} Batteries";
+    }
+
+}
+
