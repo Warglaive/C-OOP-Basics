367bc8e168f66c4f3e8cc39af6d60b11b916b7d9
warglaivee <warglaivegm@gmail.com>
2018-03-10 19:47:33 +0200

    up

---
+++ Forum.Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forum.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public ICollection<int> PostIds { get; set; }
+
+        public User(int id, string userName, string password, IEnumerable<int> postids)
+        {
+            this.Id = id;
+            this.UserName = userName;
+            this.Password = password;
+            this.PostIds = new List<int>(postids);
+        }
+    }
+}
