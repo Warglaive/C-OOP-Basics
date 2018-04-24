8db2f41e941097ea63ab4256d9b86685e79b826a
warglaivee <warglaivegm@gmail.com>
2018-04-24 17:28:57 +0300

    up

--- ExamPrep/DungeonsAndCodeWizards/Characters/Character.cs
+++ ExamPrep/DungeonsAndCodeWizards/Characters/Character.cs
@@ -1,10 +1,48 @@
 ﻿using DungeonsAndCodeWizards.Contracts;
+using DungeonsAndCodeWizards.Items;
 
 namespace DungeonsAndCodeWizards.Characters
 {
     public abstract class Character : ICharacter
     {
-        public bool IsAlive { get; set; }
+        public string Name { get; }
+        public double BaseHealth { get; }
         public double Health { get; set; }
+        public double BaseArmor { get; }
+        public double Armor { get; set; }
+        public double AbilityPoints { get; }
+        public IBag Bag { get; }
+        public Faction Faction { get; }
+        public bool IsAlive { get; set; }
+        public double RestHealMultiplier { get; }
+        public void TakeDamage(double hitPoints)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rest()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UseItem(Item item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UseItemOn(Item item, Character character)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GiveCharacterItem(Item item, Character character)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ReceiveItem(Item item)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- ExamPrep/DungeonsAndCodeWizards/Contracts/ICharacter.cs
+++ ExamPrep/DungeonsAndCodeWizards/Contracts/ICharacter.cs
@@ -1,8 +1,26 @@
-﻿namespace DungeonsAndCodeWizards.Contracts
+﻿using DungeonsAndCodeWizards.Characters;
+using DungeonsAndCodeWizards.Items;
+
+namespace DungeonsAndCodeWizards.Contracts
 {
     public interface ICharacter
     {
-        bool IsAlive { get; set; }
+        string Name { get; }
+        double BaseHealth { get; }
         double Health { get; set; }
+        double BaseArmor { get; }
+        double Armor { get; set; }
+        double AbilityPoints { get; }
+        IBag Bag { get; }
+        Faction Faction { get; }
+        bool IsAlive { get; set; }
+        double RestHealMultiplier { get; }
+        //behavior
+        void TakeDamage(double hitPoints);
+        void Rest();
+        void UseItem(Item item);
+        void UseItemOn(Item item, Character character);
+        void GiveCharacterItem(Item item, Character character);
+        void ReceiveItem(Item item);
     }
 }
